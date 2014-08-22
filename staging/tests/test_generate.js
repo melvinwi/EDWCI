e47ceead444685d61df9063acdfabd4551dcb8ba
Dan Morrison <dan@danm.it>
2014-08-22 10:24:00 +1000

    Added type uniqueidentifier
    Prevent "()" from being appended during generation.

--- staging/tests/test_generate.js
+++ staging/tests/test_generate.js
@@ -35,7 +35,7 @@ function test_generate(artefactName, object, schema, dbType) // Constructor
 
     	sql += '`'+row.COLUMN+'` ';
 
-    	if (row.TYPE.toLowerCase()!='datetime' && row.TYPE.toLowerCase()!='date' && row.TYPE.toLowerCase()!='int' && row.TYPE.toLowerCase()!='bit'  && row.TYPE.toLowerCase()!='money' && row.TYPE.toLowerCase()!='tinyint') {
+    	if (row.TYPE.toLowerCase()!='datetime' && row.TYPE.toLowerCase()!='date' && row.TYPE.toLowerCase()!='int' && row.TYPE.toLowerCase()!='bit'  && row.TYPE.toLowerCase()!='money' && row.TYPE.toLowerCase()!='tinyint' && row.TYPE.toLowerCase()!='uniqueidentifier') {
     		sql += row.TYPE+'('+row.LENGTH+') ';
     	}
     	else {
