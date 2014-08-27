d406d17067d9e493f4f95301c83c32da88cdd49b
Matt Duncombe <matt@raktor.net>
2014-08-27 16:44:06 +1000

    Added DimProduct staging, transform & dimension

---
+++ transformations/lumo_TRANSFORM_DimProduct.sql
@@ -0,0 +1,30 @@
+CREATE PROCEDURE lumo.TRANSFORM_DimProduct
+@TaskExecutionInstanceID INT
+,@LatestSuccessfulTaskExecutionInstanceID INT
+AS
+BEGIN
+
+--Get LatestSuccessfulTaskExecutionInstanceID
+IF  @LatestSuccessfulTaskExecutionInstanceID IS NULL
+BEGIN
+EXEC DW_Utility.config.GetLatestSuccessfulTaskExecutionInstanceID
+@TaskExecutionInstanceID = @TaskExecutionInstanceID
+, @LatestSuccessfulTaskExecutionInstanceID  = @LatestSuccessfulTaskExecutionInstanceID OUTPUT
+END
+--/
+
+	INSERT INTO lumo.DimProduct (
+		DimProduct.ProductName,
+		DimProduct.ProductDesc)
+	  SELECT
+		Product.ProductName,
+		Product.ProductDesc
+	  FROM lumo.Product WHERE Product.Meta_LatestUpdate_TaskExecutionInstanceId  > @LatestSuccessfulTaskExecutionInstanceID;
+
+SELECT 0 AS ExtractRowCount,
+@@ROWCOUNT AS InsertRowCount,
+0 AS UpdateRowCount,
+0 AS DeleteRowCount,
+0 AS ErrorRowCount;
+
+END;
