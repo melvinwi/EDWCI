e346af689ec5ab55cced9530f63c45fbc3d4a6c9
Matt Duncombe <matt@raktor.net>
2014-08-29 15:06:22 +1000

    Incorporated Melvin's changes, and refactoring pt 2

--- transformations/lumo_TRANSFORM_DimAccount.sql
+++ transformations/lumo_TRANSFORM_DimAccount.sql
@@ -25,7 +25,9 @@ END
 		DimAccount.MyAccountStatus,
 		DimAccount.CreationDate,
 		DimAccount.AccountStatus,
-		DimAccount.PaymentMethod)
+		DimAccount.PaymentMethod,
+		DimAccount.InvoiceDeliveryMethod,
+		DimAccount.CreditControlStatus)
 	  SELECT
 		CAST( nc_client.seq_party_id AS int),
 		CASE WHEN ISNUMERIC (crm_party.party_code) = 1 THEN CAST( crm_party.party_code AS int) END,
@@ -37,8 +39,10 @@ END
 		CAST(CASE nc_client.cz_registered WHEN 'Y' THEN 'Registered' ELSE 'Not Registered' END AS nvarchar(14)),
 		nc_client.insert_datetime,
 		CAST (CASE WHEN _accountStatus.StatusOpen = 1 THEN 'Open' WHEN _accountStatus.StatusPending = 1 THEN 'Pending' WHEN _accountStatus.StatusError = 1 THEN 'Error' ELSE 'Closed' END AS nchar (10)),
-		CAST (CASE nc_client.seq_pay_method_id WHEN 14 THEN 'Cheque' WHEN 17 THEN 'Direct Credit' WHEN 18 THEN 'Direct Debit' WHEN 22 THEN 'Credit Card' ELSE NULL END AS NVARCHAR(20))
-	  FROM lumo.nc_client INNER JOIN lumo.crm_party ON nc_client.seq_party_id = crm_party.seq_party_id INNER JOIN accountStatus AS _accountStatus ON _accountStatus.seq_party_id = nc_client.seq_party_id WHERE (crm_party.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID OR nc_client.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID OR _accountStatus.Meta_HasChanged = 1);
+		CAST (CASE nc_client.seq_pay_method_id WHEN 14 THEN 'Cheque' WHEN 17 THEN 'Direct Credit' WHEN 18 THEN 'Direct Debit' WHEN 22 THEN 'Credit Card' ELSE NULL END AS NVARCHAR(20)),
+		CAST( nc_inv_deliver_mode.inv_del_mode_desc AS nvarchar(50)),
+		CAST( nc_credit_control_status.seq_credit_status_desc AS nvarchar(50))
+	  FROM lumo.nc_client INNER JOIN lumo.crm_party ON nc_client.seq_party_id = crm_party.seq_party_id INNER JOIN accountStatus AS _accountStatus ON _accountStatus.seq_party_id = nc_client.seq_party_id LEFT JOIN lumo.nc_credit_control_status ON nc_credit_control_status.seq_credit_status_id = nc_client.seq_credit_status_id LEFT JOIN lumo.nc_inv_deliver_mode ON nc_inv_deliver_mode.seq_inv_del_mode_id = nc_client.seq_inv_del_mode_id WHERE (crm_party.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID OR nc_client.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID OR _accountStatus.Meta_HasChanged = 1);
 
 SELECT 0 AS ExtractRowCount,
 @@ROWCOUNT AS InsertRowCount,
