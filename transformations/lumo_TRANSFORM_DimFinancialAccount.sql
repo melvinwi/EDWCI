5ecd889bc2fcede8cc2df73f34f13e3bd186f8bc
Matt Duncombe <matt@raktor.net>
2014-09-10 15:27:57 +1000

    Transform [#456] DimFinancialAccount

---
+++ transformations/lumo_TRANSFORM_DimFinancialAccount.sql
@@ -0,0 +1,38 @@
+CREATE PROCEDURE lumo.TRANSFORM_DimFinancialAccount
+@TaskExecutionInstanceID INT
+,@LatestSuccessfulTaskExecutionInstanceID INT
+AS
+BEGIN
+
+--Get LatestSuccessfulTaskExecutionInstanceID
+IF  @LatestSuccessfulTaskExecutionInstanceID IS NULL
+BEGIN
+EXEC DW_Utility.config.GetLatestSuccessfulTaskExecutionInstanceID
+@TaskExecutionInstanceID = @TaskExecutionInstanceID
+, @LatestSuccessfulTaskExecutionInstanceID  = @LatestSuccessfulTaskExecutionInstanceID OUTPUT
+END
+--/
+
+	INSERT INTO lumo.DimFinancialAccount (
+		DimFinancialAccount.FinancialAccountKey,
+		DimFinancialAccount.FinancialAccountName,
+		DimFinancialAccount.FinancialAccountType,
+		DimFinancialAccount.Level1Name,
+		DimFinancialAccount.Level2Name,
+		DimFinancialAccount.Level3Name)
+	  SELECT
+		CAST( ar_account.seq_account_id AS int),
+		CAST( ar_account.account_desc AS nvarchar(100)),
+		CAST( ar_account.dr_cr AS nchar(10)),
+		CAST( ar_account.account_desc AS nvarchar(100)),
+		CAST( ar_account_group.account_group_desc AS nvarchar(100)),
+		/* ar_account.account_desc */ N'Lumo Energy'
+	  FROM lumo.ar_account INNER JOIN lumo.ar_account_group ON ar_account_group.account_group_id = ar_account.account_group_id WHERE ar_account.Meta_LatestUpdate_TaskExecutionInstanceId  > @LatestSuccessfulTaskExecutionInstanceID OR ar_account_group.Meta_LatestUpdate_TaskExecutionInstanceId  > @LatestSuccessfulTaskExecutionInstanceID;
+
+SELECT 0 AS ExtractRowCount,
+@@ROWCOUNT AS InsertRowCount,
+0 AS UpdateRowCount,
+0 AS DeleteRowCount,
+0 AS ErrorRowCount;
+
+END;
