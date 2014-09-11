e310ed046d22e6def8a9c23f9c84390a65a70719
Matt Duncombe <matt@raktor.net>
2014-09-11 11:26:19 +1000

    DimVersion transform (BROKEN)
    The transform works - but fails testing as it only selects from a view
    and has no tables to load. Committing so Leigh can test with the
    framework.

---
+++ transformations/lumo_TRANSFORM_DimVersion.sql
@@ -0,0 +1,30 @@
+CREATE PROCEDURE lumo.TRANSFORM_DimVersion
+@TaskExecutionInstanceID INT
+,@LatestSuccessfulTaskExecutionInstanceID INT
+AS
+BEGIN
+
+--Get LatestSuccessfulTaskExecutionInstanceID
+IF  @LatestSuccessfulTaskExecutionInstanceID IS NULL
+BEGIN
+EXEC DW_Utility.config.GetLatestSuccessfulTaskExecutionInstanceID
+@TaskExecutionInstanceID = @TaskExecutionInstanceID
+, @LatestSuccessfulTaskExecutionInstanceID  = @LatestSuccessfulTaskExecutionInstanceID OUTPUT
+END
+--/
+
+	INSERT INTO lumo.DimVersion (
+		DimVersion.VersionKey,
+		DimVersion.VersionName)
+	  SELECT
+		_Versions.VersionKey,
+		_Versions.VersionName
+	  FROM lumo.Versions AS _Versions;
+
+SELECT 0 AS ExtractRowCount,
+@@ROWCOUNT AS InsertRowCount,
+0 AS UpdateRowCount,
+0 AS DeleteRowCount,
+0 AS ErrorRowCount;
+
+END;
