e3c832973eb05bf40c86c78283b53f84c4fca511
Leigh Hunter <leigh.hunter@gmail.com>
2014-09-16 18:34:48 +1000

    [#DT-437] [#DT-438] [#DT-445] [#DT-446]

---
+++ transformations/lumo_TRANSFORM_FactMarketingCampaignActivity.sql
@@ -0,0 +1,40 @@
+CREATE PROCEDURE lumo.TRANSFORM_FactMarketingCampaignActivity
+@TaskExecutionInstanceID INT
+,@LatestSuccessfulTaskExecutionInstanceID INT
+AS
+BEGIN
+
+--Get LatestSuccessfulTaskExecutionInstanceID
+IF  @LatestSuccessfulTaskExecutionInstanceID IS NULL
+BEGIN
+EXEC DW_Utility.config.GetLatestSuccessfulTaskExecutionInstanceID
+@TaskExecutionInstanceID = @TaskExecutionInstanceID
+, @LatestSuccessfulTaskExecutionInstanceID  = @LatestSuccessfulTaskExecutionInstanceID OUTPUT
+END
+--/
+
+	INSERT INTO lumo.FactMarketingCampaignActivity (
+		FactMarketingCampaignActivity.MarketingCampaignId,
+		FactMarketingCampaignActivity.MarketingOfferId,
+		FactMarketingCampaignActivity.CustomerId,
+		FactMarketingCampaignActivity.ActivityDateId,
+		FactMarketingCampaignActivity.ActivityTime,
+		FactMarketingCampaignActivity.ActivityType,
+		FactMarketingCampaignActivity.MarketingCompaignActivityKey)
+	  SELECT
+		csv_FactMarketingCampaignActivity.MarketingCampaignId,
+		csv_FactMarketingCampaignActivity.MarketingOfferId,
+		_DimCustomer.CustomerId,
+		REPLACE( csv_FactMarketingCampaignActivity.ActivityDateId , '-', ''),
+		csv_FactMarketingCampaignActivity.ActivityTime,
+		csv_FactMarketingCampaignActivity.ActivityType,
+		CAST(CONCAT( csv_FactMarketingCampaignActivity.MarketingCompaignActivityKey, '-',csv_FactMarketingCampaignActivity.ActivityDateId, '-', csv_FactMarketingCampaignActivity.ActivityTime ) AS nvarchar(255))
+	  FROM /* Staging */ lumo.csv_FactMarketingCampaignActivity INNER JOIN /* Dimensional */ staticdims.DimCustomer as _DimCustomer ON _DimCustomer.CustomerKey = csv_FactMarketingCampaignActivity.CustomerKey AND _DimCustomer.Meta_IsCurrent = 1;
+
+SELECT 0 AS ExtractRowCount,
+@@ROWCOUNT AS InsertRowCount,
+0 AS UpdateRowCount,
+0 AS DeleteRowCount,
+0 AS ErrorRowCount;
+
+END;
