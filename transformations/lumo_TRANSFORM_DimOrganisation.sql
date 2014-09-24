58b0b77589a5c4e73e95e15edc44f313b9554a64
melvinwi <melvin.widodo@lumoenergy.com.au>
2014-09-24 16:52:31 +1000

    DT-495

---
+++ transformations/lumo_TRANSFORM_DimOrganisation.sql
@@ -0,0 +1,51 @@
+CREATE PROCEDURE lumo.TRANSFORM_DimOrganisation
+@TaskExecutionInstanceID INT
+,@LatestSuccessfulTaskExecutionInstanceID INT
+AS
+BEGIN
+
+--Get LatestSuccessfulTaskExecutionInstanceID
+IF  @LatestSuccessfulTaskExecutionInstanceID IS NULL
+BEGIN
+EXEC DW_Utility.config.GetLatestSuccessfulTaskExecutionInstanceID
+@TaskExecutionInstanceID = @TaskExecutionInstanceID
+, @LatestSuccessfulTaskExecutionInstanceID  = @LatestSuccessfulTaskExecutionInstanceID OUTPUT
+END
+--/
+
+	;WITH _Level3 AS (SELECT Party.seq_party_id OrgId, Party.party_code OrgCode, Party.party_name OrgName, hrc.element_struct_code StructureName, hrc.parent_id ParentId, hrc.parent_element_struct_code ParentStructureName, MAX(CASE WHEN party.Meta_LatestUpdate_TaskExecutionInstanceId >= @LatestSuccessfulTaskExecutionInstanceID OR hrc.Meta_LatestUpdate_TaskExecutionInstanceId >= @LatestSuccessfulTaskExecutionInstanceID THEN 1 ELSE 0 END) AS Meta_HasChanged FROM lumo.Crm_party party INNER JOIN lumo.Crm_element_hierarchy hrc ON hrc.element_id = seq_party_id WHERE element_struct_code IN ('COMPANY') GROUP BY Party.seq_party_id, Party.party_code, Party.party_name, hrc.element_struct_code, hrc.parent_id, hrc.parent_element_struct_code), _Level2 AS (SELECT Party.seq_party_id OrgId, Party.party_code OrgCode, Party.party_name OrgName, hrc.element_struct_code StructureName, hrc.parent_id ParentId, hrc.parent_element_struct_code ParentStructureName, MAX(CASE WHEN party.Meta_LatestUpdate_TaskExecutionInstanceId >= @LatestSuccessfulTaskExecutionInstanceID OR hrc.Meta_LatestUpdate_TaskExecutionInstanceId >= @LatestSuccessfulTaskExecutionInstanceID THEN 1 ELSE 0 END) AS Meta_HasChanged FROM lumo.Crm_party party INNER JOIN lumo.Crm_element_hierarchy hrc ON hrc.element_id = seq_party_id WHERE element_struct_code IN ('SALESGRP') GROUP BY Party.seq_party_id, Party.party_code, Party.party_name, hrc.element_struct_code, hrc.parent_id, hrc.parent_element_struct_code), _Level1 AS (SELECT Party.seq_party_id OrgId, Party.party_code OrgCode, Party.party_name OrgName, hrc.element_struct_code StructureName, hrc.parent_id ParentId, hrc.parent_element_struct_code ParentStructureName, MAX(CASE WHEN party.Meta_LatestUpdate_TaskExecutionInstanceId >= @LatestSuccessfulTaskExecutionInstanceID OR hrc.Meta_LatestUpdate_TaskExecutionInstanceId >= @LatestSuccessfulTaskExecutionInstanceID THEN 1 ELSE 0 END) AS Meta_HasChanged FROM lumo.Crm_party party INNER JOIN lumo.Crm_element_hierarchy hrc ON hrc.element_id = seq_party_id WHERE element_struct_code IN ('SALESDEPT') GROUP BY Party.seq_party_id, Party.party_code, Party.party_name, hrc.element_struct_code, hrc.parent_id, hrc.parent_element_struct_code )
+	INSERT INTO lumo.DimOrganisation (
+		DimOrganisation.OrganisationId,
+		DimOrganisation.OrganisationCode,
+		DimOrganisation.OrganisationKey,
+		DimOrganisation.Level1Code,
+		DimOrganisation.Level1Name,
+		DimOrganisation.Level1StructureName,
+		DimOrganisation.Level2Code,
+		DimOrganisation.Level2Name,
+		DimOrganisation.Level2StructureName,
+		DimOrganisation.Level3Code,
+		DimOrganisation.Level3Name,
+		DimOrganisation.Level3StructureName)
+	  SELECT
+		CAST( _Level1.OrgId AS int),
+		CAST( _Level1.OrgCode AS nchar(10)),
+		CAST( _Level1.OrgId AS int),
+		CAST( _Level1.OrgCode AS nchar(10)),
+		CAST( _Level1.OrgName AS nvarchar(100)),
+		CAST( _Level1.StructureName AS nchar(10)),
+		CAST( _Level2.OrgCode AS nchar(10)),
+		CAST( _Level2.OrgName AS nvarchar(100)),
+		CAST( _Level2.StructureName AS nchar(10)),
+		CAST( _Level3.OrgCode AS nchar(10)),
+		CAST( _Level3.OrgName AS nvarchar(100)),
+		CAST( _Level3.StructureName AS nchar(10))
+	  FROM _Level3 INNER JOIN _Level2 ON _Level2.ParentId = _Level3.OrgId INNER JOIN _Level1 ON _Level1.ParentId = _Level2.OrgId WHERE (_Level1.Meta_HasChanged = 1) OR ( _Level2.Meta_HasChanged = 1) OR (_Level3.Meta_HasChanged = 1);
+
+SELECT 0 AS ExtractRowCount,
+@@ROWCOUNT AS InsertRowCount,
+0 AS UpdateRowCount,
+0 AS DeleteRowCount,
+0 AS ErrorRowCount;
+
+END;
