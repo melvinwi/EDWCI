619f2e1ab1d441601613dbb95827810f08c3b9a5
melvinwi <melvin.widodo@lumoenergy.com.au>
2014-09-25 16:04:44 +1000

    DT-495 - Transformation for DimOrganisation provisioned
    Mapping files
    Data Samples
    Transformation files

--- transformations/lumo_TRANSFORM_DimOrganisation.sql
+++ transformations/lumo_TRANSFORM_DimOrganisation.sql
@@ -13,32 +13,22 @@ EXEC DW_Utility.config.GetLatestSuccessfulTaskExecutionInstanceID
 END
 --/
 
-	;WITH _Level3 AS (SELECT Party.seq_party_id OrgId, Party.party_code OrgCode, Party.party_name OrgName, hrc.element_struct_code StructureName, hrc.parent_id ParentId, hrc.parent_element_struct_code ParentStructureName, MAX(CASE WHEN party.Meta_LatestUpdate_TaskExecutionInstanceId >= @LatestSuccessfulTaskExecutionInstanceID OR hrc.Meta_LatestUpdate_TaskExecutionInstanceId >= @LatestSuccessfulTaskExecutionInstanceID THEN 1 ELSE 0 END) AS Meta_HasChanged FROM lumo.Crm_party party INNER JOIN lumo.Crm_element_hierarchy hrc ON hrc.element_id = seq_party_id WHERE element_struct_code IN ('COMPANY') GROUP BY Party.seq_party_id, Party.party_code, Party.party_name, hrc.element_struct_code, hrc.parent_id, hrc.parent_element_struct_code), _Level2 AS (SELECT Party.seq_party_id OrgId, Party.party_code OrgCode, Party.party_name OrgName, hrc.element_struct_code StructureName, hrc.parent_id ParentId, hrc.parent_element_struct_code ParentStructureName, MAX(CASE WHEN party.Meta_LatestUpdate_TaskExecutionInstanceId >= @LatestSuccessfulTaskExecutionInstanceID OR hrc.Meta_LatestUpdate_TaskExecutionInstanceId >= @LatestSuccessfulTaskExecutionInstanceID THEN 1 ELSE 0 END) AS Meta_HasChanged FROM lumo.Crm_party party INNER JOIN lumo.Crm_element_hierarchy hrc ON hrc.element_id = seq_party_id WHERE element_struct_code IN ('SALESGRP') GROUP BY Party.seq_party_id, Party.party_code, Party.party_name, hrc.element_struct_code, hrc.parent_id, hrc.parent_element_struct_code), _Level1 AS (SELECT Party.seq_party_id OrgId, Party.party_code OrgCode, Party.party_name OrgName, hrc.element_struct_code StructureName, hrc.parent_id ParentId, hrc.parent_element_struct_code ParentStructureName, MAX(CASE WHEN party.Meta_LatestUpdate_TaskExecutionInstanceId >= @LatestSuccessfulTaskExecutionInstanceID OR hrc.Meta_LatestUpdate_TaskExecutionInstanceId >= @LatestSuccessfulTaskExecutionInstanceID THEN 1 ELSE 0 END) AS Meta_HasChanged FROM lumo.Crm_party party INNER JOIN lumo.Crm_element_hierarchy hrc ON hrc.element_id = seq_party_id WHERE element_struct_code IN ('SALESDEPT') GROUP BY Party.seq_party_id, Party.party_code, Party.party_name, hrc.element_struct_code, hrc.parent_id, hrc.parent_element_struct_code )
+	;WITH _Level4 AS  (  SELECT Party.seq_party_id OrgId, Party.party_code OrgCode, Party.party_name OrgName, hrc.element_struct_code StructureName, hrc.parent_id ParentId, hrc.parent_element_struct_code ParentStructureName  , MAX(CASE WHEN party.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID OR hrc.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID THEN 1 ELSE 0 END) AS Meta_HasChanged   FROM lumo.Crm_party party    INNER JOIN lumo.Crm_element_hierarchy hrc ON hrc.element_id = seq_party_id   WHERE element_struct_code = 'GROUP'  GROUP BY Party.seq_party_id, Party.party_code, Party.party_name, hrc.element_struct_code, hrc.parent_id, hrc.parent_element_struct_code ) , _Level3 AS  (  SELECT Party.seq_party_id OrgId, Party.party_code OrgCode, Party.party_name OrgName, hrc.element_struct_code StructureName, hrc.parent_id ParentId, hrc.parent_element_struct_code ParentStructureName  , MAX(CASE WHEN party.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID OR hrc.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID THEN 1 ELSE 0 END) AS Meta_HasChanged   FROM lumo.Crm_party party    INNER JOIN lumo.Crm_element_hierarchy hrc ON hrc.element_id = seq_party_id   WHERE element_struct_code = 'COMPANY'  GROUP BY Party.seq_party_id, Party.party_code, Party.party_name, hrc.element_struct_code, hrc.parent_id, hrc.parent_element_struct_code ) , _Level2 AS  (  SELECT Party.seq_party_id OrgId, Party.party_code OrgCode, Party.party_name OrgName, hrc.element_struct_code StructureName, hrc.parent_id ParentId, hrc.parent_element_struct_code ParentStructureName  , MAX(CASE WHEN party.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID OR hrc.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID THEN 1 ELSE 0 END) AS Meta_HasChanged   FROM lumo.Crm_party party    INNER JOIN lumo.Crm_element_hierarchy hrc ON hrc.element_id = seq_party_id   WHERE element_struct_code = 'SALESGRP' OR element_struct_code = 'DEPARTMENT'  GROUP BY Party.seq_party_id, Party.party_code, Party.party_name, hrc.element_struct_code, hrc.parent_id, hrc.parent_element_struct_code ) , _Level1 AS  (  SELECT Party.seq_party_id OrgId, Party.party_code OrgCode, Party.party_name OrgName, hrc.element_struct_code StructureName, hrc.parent_id ParentId, hrc.parent_element_struct_code ParentStructureName  , MAX(CASE WHEN party.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID OR hrc.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID THEN 1 ELSE 0 END) AS Meta_HasChanged   FROM lumo.Crm_party party    INNER JOIN lumo.Crm_element_hierarchy hrc ON hrc.element_id = seq_party_id   WHERE element_struct_code = 'SALESDEPT'  GROUP BY Party.seq_party_id, Party.party_code, Party.party_name, hrc.element_struct_code, hrc.parent_id, hrc.parent_element_struct_code  )
 	INSERT INTO lumo.DimOrganisation (
 		DimOrganisation.OrganisationCode,
 		DimOrganisation.OrganisationKey,
-		DimOrganisation.Level1Code,
 		DimOrganisation.Level1Name,
-		DimOrganisation.Level1StructureName,
-		DimOrganisation.Level2Code,
 		DimOrganisation.Level2Name,
-		DimOrganisation.Level2StructureName,
-		DimOrganisation.Level3Code,
 		DimOrganisation.Level3Name,
-		DimOrganisation.Level3StructureName)
+		DimOrganisation.Level4Name)
 	  SELECT
 		CAST( _Level1.OrgCode AS nchar(10)),
 		CAST( _Level1.OrgId AS int),
-		CAST( _Level1.OrgCode AS nchar(10)),
 		CAST( _Level1.OrgName AS nvarchar(100)),
-		CAST( _Level1.StructureName AS nchar(10)),
-		CAST( _Level2.OrgCode AS nchar(10)),
 		CAST( _Level2.OrgName AS nvarchar(100)),
-		CAST( _Level2.StructureName AS nchar(10)),
-		CAST( _Level3.OrgCode AS nchar(10)),
 		CAST( _Level3.OrgName AS nvarchar(100)),
-		CAST( _Level3.StructureName AS nchar(10))
-	  FROM _Level3 INNER JOIN _Level2 ON _Level2.ParentId = _Level3.OrgId INNER JOIN _Level1 ON _Level1.ParentId = _Level2.OrgId WHERE (_Level1.Meta_HasChanged = 1 OR _Level2.Meta_HasChanged = 1 OR _Level3.Meta_HasChanged = 1);
+		CAST( _Level4.OrgName AS nvarchar(100))
+	  FROM _Level4  LEFT JOIN _Level3 ON _Level3.ParentId = _Level4.OrgId  LEFT JOIN _Level2 ON _Level2.ParentId = _Level3.OrgId  LEFT JOIN _Level1 ON _Level1.ParentId = _Level2.OrgId WHERE (_Level1.Meta_HasChanged = 1 OR _Level2.Meta_HasChanged = 1 OR _Level3.Meta_HasChanged = 1 OR _Level4.Meta_HasChanged = 1);
 
 SELECT 0 AS ExtractRowCount,
 @@ROWCOUNT AS InsertRowCount,
