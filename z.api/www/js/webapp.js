35c7b4afa8abe12482235cd69e87ceb5af7cb9ce
Leigh Hunter <leigh.hunter@gmail.com>
2014-10-01 12:01:51 +1000

    initial api and web app
    ready for integration

---
+++ z.api/www/js/webapp.js
@@ -0,0 +1,92 @@
+var webapp = angular.module('webapp', ['ngResource', 'ngRoute'])
+
+
+// global variables
+var token = '';
+var username = '';
+
+// header
+webapp.controller('AppController', function AppController($scope, $resource) {
+
+
+
+// login components controller logic
+
+	$scope.showLoginController = true;
+
+    $scope.loginUser = function(login) {
+
+		var Authenticate = $resource('/auth/:username/:password', {username: login.username, password: login.password});
+		Authenticate.get(function(object) {
+			
+			if (object.ERROR) {
+				$scope.status = object.ERROR;
+				token = object.token;
+			}
+			else {
+				$scope.status = '';
+				token = object.token;
+				username = object.username;
+
+				$scope.showLoginController = false;
+				$scope.showAccountController = true;
+			}
+		});
+    };
+
+
+
+// account component controller logic
+
+	var cName = '';
+	var cRating = '';
+
+	// init
+	$scope.customerName = '...';
+	$scope.customerRating = '...';
+
+    $scope.getCustomerRating = function(account) {
+
+    	
+		var CustomerRating = $resource('/api/:apiName/:token?id=:account', {apiName: 'CustomerRating', token: token, account: account.id});
+		CustomerRating.get(function(object) {
+			
+			$scope.customerName = '...';
+			$scope.customerRating = '...';
+
+			if (object.ERROR!=undefined) {
+				if (object.ERROR.indexOf('has expired')!=-1) {
+					$scope.customerRatingStatus = 'Session has expired - please Login';
+
+					setTimeout(function() {
+						$scope.showLoginController = true;
+						$scope.showAccountController = false;
+					}, 2000);
+				}
+				else {
+					$scope.customerRatingStatus = object.ERROR;
+				}
+
+				$scope.customerName = '...';
+				$scope.customerRating = '...';
+			}
+			else {
+				if (object.CustomerRating[0].CustomerName!=undefined) {
+					
+					cName = object.CustomerRating[0].CustomerName.toString();
+					cRating = object.CustomerRating[0].CustomerRating.toString();
+					$scope.customerName = cName;
+					$scope.customerRating = cRating;
+				}
+				else {
+					$scope.status = 'Account Id was not found';
+					$scope.customerName = '';
+					$scope.customerRating = '';
+				}
+			}
+		});
+        
+    };
+
+
+});
