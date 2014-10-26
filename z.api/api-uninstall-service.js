52fdd3680a6efab4bceecfdb5c41dadf322d72be
Leigh Hunter <leigh.hunter@gmail.com>
2014-10-27 10:36:34 +1100

    windows service installation

---
+++ z.api/api-uninstall-service.js
@@ -0,0 +1,20 @@
+var Service = require('node-windows').Service;
+
+// service name 
+var SERVICE_NAME = 'Data Transformation API Server';
+
+
+// Create a new service object
+var svc = new Service({
+  name: SERVICE_NAME,
+  script: require('path').join(__dirname,'server.js')
+});
+
+// Listen for the "uninstall" event so we know when it's done.
+svc.on('uninstall',function(){
+  console.log('Uninstall complete.');
+  console.log('The service exists: ',svc.exists);
+});
+
+// Uninstall the service.
+svc.uninstall();
