f8ce96a379c0ab9a1a78522a40bcb6bfc4236f26
Dan Morrison <dan@danm.it>
2014-10-27 15:03:59 +1100

    Add communication method and inferred gender [#594]

--- views/vActivity.sql
+++ views/vActivity.sql
@@ -1,78 +1,89 @@
 CREATE VIEW Views.vActivity
 AS
 WITH   dimMarketingCampaign
-       AS (SELECT FactMarketingCampaignActivity.ActivityTypeId,
+       AS (SELECT DimActivityType.ActivityTypeKey,
                   DimMarketingCampaign.MarketingCampaignShortDesc,
                   DimMarketingCampaign.MarketingCampaignStartDate,
-                  row_number() OVER (PARTITION BY FactMarketingCampaignActivity.ActivityTypeId ORDER BY DimMarketingCampaign.MarketingCampaignStartDate DESC) AS recency
-           FROM   DW_Dimensional.DW.FactMarketingCampaignActivity
+                  row_number() OVER (PARTITION BY DimActivityType.ActivityTypeKey ORDER BY DimMarketingCampaign.MarketingCampaignStartDate DESC) AS recency
+           FROM   DW_Dimensional.DW.DimActivityType
+           INNER  JOIN DW_Dimensional.DW.FactMarketingCampaignActivity ON FactMarketingCampaignActivity.ActivityTypeId = DimActivityType.ActivityTypeId
            INNER  JOIN DW_Dimensional.DW.DimMarketingCampaign ON DimMarketingCampaign.MarketingCampaignId = FactMarketingCampaignActivity.MarketingCampaignId),
        dimAccount
-       AS (SELECT FactCustomerAccount.CustomerId,
+       AS (SELECT DimCustomer.CustomerKey,
                   DimAccount.AccountKey,
                   DimAccount.AccountClosedDate,
-                  row_number() OVER (PARTITION BY FactCustomerAccount.CustomerId ORDER BY DimAccount.Meta_EffectiveStartDate DESC) AS recency
-           FROM   DW_Dimensional.DW.FactCustomerAccount
+                  row_number() OVER (PARTITION BY DimCustomer.CustomerKey ORDER BY DimAccount.Meta_EffectiveStartDate DESC) AS recency
+           FROM   DW_Dimensional.DW.DimCustomer
+           INNER  JOIN DW_Dimensional.DW.FactCustomerAccount ON FactCustomerAccount.CustomerId = DimCustomer.CustomerId
            INNER  JOIN DW_Dimensional.DW.DimAccount ON DimAccount.AccountId = FactCustomerAccount.AccountId),
        factContract
        AS (SELECT DimAccount.AccountKey,
                   MAX(FactContract.ContractStartDateId) AS MaxContractStartDateId
-           FROM   DW_Dimensional.DW.FactContract
-           INNER  JOIN DW_Dimensional.DW.DimAccount ON DimAccount.AccountId = FactContract.AccountId
+           FROM   DW_Dimensional.DW.DimAccount
+           INNER  JOIN DW_Dimensional.DW.FactContract ON DimAccount.AccountId = FactContract.AccountId
            GROUP  BY DimAccount.AccountKey)
 SELECT -- DimCustomer
-       DimCustomer.CustomerCode,
-       ISNULL(NULLIF(DimCustomer.Title, ''), '{U}') AS Title,
-       ISNULL(NULLIF(DimCustomer.FirstName, ''), '{Unknown}') AS FirstName,
-       ISNULL(NULLIF(DimCustomer.MiddleInitial, ''), '{Unknown}') AS MiddleInitial,
-       ISNULL(NULLIF(DimCustomer.LastName, ''), '{Unknown}') AS LastName,
-       ISNULL(NULLIF(DimCustomer.PartyName, ''), '{Unknown}') AS PartyName,
-       ISNULL(NULLIF(DimCustomer.ResidentialAddressLine1, ''), '{Unknown}') AS ResidentialAddressLine1,
-       ISNULL(NULLIF(DimCustomer.ResidentialSuburb, ''), '{Unknown}') AS ResidentialSuburb,
-       ISNULL(NULLIF(DimCustomer.ResidentialPostcode, ''), '{Un}') AS ResidentialPostcode,
-       ISNULL(NULLIF(DimCustomer.ResidentialState, ''), '{U}') AS ResidentialState,
-       ISNULL(NULLIF(DimCustomer.ResidentialStateAsProvided, ''), '{U}') AS ResidentialStateAsProvided,
-       ISNULL(NULLIF(DimCustomer.PrimaryPhone, ''), '{Unknown}') AS PrimaryPhone,
-       ISNULL(NULLIF(DimCustomer.PrimaryPhoneType, ''),'{Unk}') AS PrimaryPhoneType,
-       ISNULL(NULLIF(DimCustomer.SecondaryPhone, ''), '{Unknown}') AS SecondaryPhone,
-       ISNULL(NULLIF(DimCustomer.SecondaryPhoneType, ''),'{Unk}') AS SecondaryPhoneType,
-       ISNULL(NULLIF(DimCustomer.MobilePhone, ''), '{Unknown}') AS MobilePhone,
-       ISNULL(NULLIF(DimCustomer.Email, ''), '{Unknown}') AS Email,
-       DimCustomer.DateOfBirth,
-       DATEDIFF(YEAR, DimCustomer.DateOfBirth, GETDATE()) AS Age,
-       ISNULL(NULLIF(DimCustomer.CustomerType, ''), '{Unknown}') AS CustomerType,
-       ISNULL(NULLIF(DimCustomer.CustomerStatus, ''),'{Unk}') AS CustomerStatus,
-       ISNULL(NULLIF(DimCustomer.OmbudsmanComplaints, ''),'{U}') AS OmbudsmanComplaints,
-       DimCustomer.CreationDate,
-       DimCustomer.JoinDate,
-       ISNULL(NULLIF(DimCustomer.PrivacyPreferredStatus, ''),'{Unknown}') AS PrivacyPreferredStatus,
+       DimCustomerCurrent.CustomerCode,
+       DimCustomerCurrent.Title,
+       DimCustomerCurrent.FirstName,
+       DimCustomerCurrent.MiddleInitial,
+       DimCustomerCurrent.LastName,
+       DimCustomerCurrent.PartyName,
+       DimCustomerCurrent.ResidentialAddressLine1,
+       DimCustomerCurrent.ResidentialSuburb,
+       DimCustomerCurrent.ResidentialPostcode,
+       DimCustomerCurrent.ResidentialState,
+       DimCustomerCurrent.ResidentialStateAsProvided,
+       DimCustomerCurrent.PrimaryPhone,
+       DimCustomerCurrent.PrimaryPhoneType,
+       DimCustomerCurrent.SecondaryPhone,
+       DimCustomerCurrent.SecondaryPhoneType,
+       DimCustomerCurrent.MobilePhone,
+       DimCustomerCurrent.Email,
+       DimCustomerCurrent.DateOfBirth,
+       DATEDIFF(YEAR, DimCustomerCurrent.DateOfBirth, GETDATE())
+       - CASE
+           WHEN DATEDIFF(DAY, GETDATE(), DATEADD(YEAR, DATEDIFF (YEAR, DimCustomerCurrent.DateOfBirth, GETDATE()), DimCustomerCurrent.DateOfBirth)) > 0 THEN 1
+           ELSE 0 -- Subtract birthdays that are yet to occur in the current year
+         END AS Age,
+       DimCustomerCurrent.CustomerType,
+       DimCustomerCurrent.CustomerStatus,
+       DimCustomerCurrent.OmbudsmanComplaints,
+       DimCustomerCurrent.CreationDate,
+       DimCustomerCurrent.JoinDate,
+       DimCustomerCurrent.PrivacyPreferredStatus,
+       DimCustomerCurrent.InferredGender,
        -- DimRepresentative
-       ISNULL(NULLIF(DimRepresentative.RepresentativePartyName, ''),'{Unknown}') AS RepresentativePartyName,
+       DimRepresentativeCurrent.RepresentativePartyName,
        -- DimOrganisation
-       ISNULL(NULLIF(DimOrganisation.OrganisationName, ''),'{Unknown}') AS OrganisationName,
-       ISNULL(NULLIF(DimOrganisation.Level1Name, ''),'{Unknown}') AS Level1Name,
-       ISNULL(NULLIF(DimOrganisation.Level2Name, ''),'{Unknown}') AS Level2Name,
-       ISNULL(NULLIF(DimOrganisation.Level3Name, ''),'{Unknown}') AS Level3Name,
-       ISNULL(NULLIF(DimOrganisation.Level4Name, ''),'{Unknown}') AS Level4Name,
+       DimOrganisationCurrent.OrganisationName,
+       DimOrganisationCurrent.Level1Name,
+       DimOrganisationCurrent.Level2Name,
+       DimOrganisationCurrent.Level3Name,
+       DimOrganisationCurrent.Level4Name,
        -- DimActivityType
-       ISNULL(NULLIF(DimActivityType.ActivityTypeCode, ''),'{Unknown}') AS ActivityTypeCode,
-       ISNULL(NULLIF(DimActivityType.ActivityTypeDesc, ''),'{Unknown}') AS ActivityTypeDesc,
+       DimActivityTypeCurrent.ActivityTypeCode,
+       DimActivityTypeCurrent.ActivityTypeDesc,
        -- FactActivity
-       CONVERT(DATE, CAST(FactActivity.ActivityDateId AS NCHAR(8)), 112) AS ActivityDate,
-       FactActivity.ActivityTime,
-       ISNULL(NULLIF(FactActivity.ActivityCategory, ''), '{Unknown}') AS ActivityCategory,
-       ISNULL(NULLIF(FactActivity.ActivityNotes, ''), '{Unknown}') AS ActivityNotes,
+       CONVERT(DATETIME2, CAST(FactActivity.ActivityDateId AS NCHAR(8)) + ' ' + CAST(FactActivity.ActivityTime AS NCHAR(16))) AS ActivityDate,
+       FactActivity.ActivityCategory,
+       FactActivity.ActivityCommunicationMethod,
+       FactActivity.ActivityNotes,
        -- DimCampaign
-       ISNULL(NULLIF(dimMarketingCampaign.MarketingCampaignShortDesc, ''), '{Unknown}') AS MarketingCampaignShortDesc,
+       dimMarketingCampaign.MarketingCampaignShortDesc,
        -- DimAccount
        dimAccount.AccountClosedDate,
        -- FactContract
        CONVERT(DATE, CAST(factContract.MaxContractStartDateId AS NCHAR(8)), 120) AS RecontractDate
 FROM   DW_Dimensional.DW.FactActivity
-INNER  JOIN DW_Dimensional.DW.DimCustomer ON DimCustomer.CustomerId = FactActivity.CustomerId
+LEFT   JOIN DW_Dimensional.DW.DimCustomer ON DimCustomer.CustomerId = FactActivity.CustomerId
+LEFT   JOIN DW_Dimensional.DW.DimCustomer AS DimCustomerCurrent ON DimCustomerCurrent.CustomerKey = DimCustomer.CustomerKey AND DimCustomerCurrent.Meta_IsCurrent = 1
 LEFT   JOIN DW_Dimensional.DW.DimRepresentative ON DimRepresentative.RepresentativeId = FactActivity.RepresentativeId
+LEFT   JOIN DW_Dimensional.DW.DimRepresentative AS DimRepresentativeCurrent ON DimRepresentativeCurrent.RepresentativeKey = DimRepresentative.RepresentativeKey AND DimRepresentative.Meta_IsCurrent = 1
 LEFT   JOIN DW_Dimensional.DW.DimOrganisation ON DimOrganisation.OrganisationId = FactActivity.OrganisationId
+LEFT   JOIN DW_Dimensional.DW.DimOrganisation AS DimOrganisationCurrent ON DimOrganisationCurrent.OrganisationKey = DimOrganisation.OrganisationKey AND DimOrganisation.Meta_IsCurrent = 1
 LEFT   JOIN DW_Dimensional.DW.DimActivityType ON DimActivityType.ActivityTypeId = FactActivity.ActivityTypeId
-LEFT   JOIN dimMarketingCampaign ON dimMarketingCampaign.ActivityTypeId = DW_Dimensional.DW.DimActivityType.ActivityTypeId AND dimMarketingCampaign.recency = 1
-LEFT   JOIN dimAccount ON dimAccount.CustomerId = FactActivity.CustomerId AND dimAccount.recency = 1
+LEFT   JOIN DW_Dimensional.DW.DimActivityType AS DimActivityTypeCurrent ON DimActivityTypeCurrent.ActivityTypeKey = DimActivityType.ActivityTypeKey AND DimActivityTypeCurrent.Meta_IsCurrent = 1
+LEFT   JOIN dimMarketingCampaign ON dimMarketingCampaign.ActivityTypeKey = DimActivityTypeCurrent.ActivityTypeKey AND dimMarketingCampaign.recency = 1
+LEFT   JOIN dimAccount ON dimAccount.CustomerKey = DimCustomerCurrent.CustomerKey AND dimAccount.recency = 1
 LEFT   JOIN factContract ON factContract.AccountKey = dimAccount.AccountKey AND CONVERT(DATE, CAST(factContract.MaxContractStartDateId AS NCHAR(8)), 120) >= dimMarketingCampaign.MarketingCampaignStartDate;
