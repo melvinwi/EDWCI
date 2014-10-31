e8ba5433425dca11103c705f5cb384dd5d84621d
danielsteinwall <Daniel.Steinwall@lumoenergy.com.au>
2014-10-31 11:31:15 +1100

    DT 601 - 604
    DimQuestion and FactSurvey amended as per Dan's request

--- transformations/lumo_TRANSFORM_FactSurvey.sql
+++ transformations/lumo_TRANSFORM_FactSurvey.sql
@@ -13,7 +13,7 @@ EXEC DW_Utility.config.GetLatestSuccessfulTaskExecutionInstanceID
 END
 --/
 
-	;WITH _CX_Monitor AS ( SELECT     CX_Monitor.QuestionDescription      ,CX_Monitor.QuestionKey     ,CASE WHEN(CASE WHEN (CHARINDEX('#', CX_Monitor.QuestionKey)) = (LEN(CX_Monitor.QuestionKey)) THEN 1 ELSE 0 END) = 0        THEN REPLACE(SUBSTRING(CX_Monitor.QuestionKey, CHARINDEX(',', CX_Monitor.QuestionKey), LEN(CX_Monitor.QuestionKey)), ',', '')        ELSE CX_Monitor.QuestionKey END AS QuestionId     ,CX_Monitor.RespondentId      ,CX_Monitor.Data      ,CX_Monitor.Meta_Insert_TaskExecutionInstanceId      ,CX_Monitor.Meta_LatestUpdate_TaskExecutionInstanceId     ,SUBSTRING(CX_Monitor.QuestionDescription,CHARINDEX(' ',CX_Monitor.QuestionDescription)+1,LEN(CX_Monitor.QuestionDescription)) AS QuestionRef       FROM [Schema].CX_Monitor       WHERE 1=1     AND  CHARINDEX('Q',CX_Monitor.QuestionDescription) > 0      AND  CHARINDEX('x.',CX_Monitor.QuestionDescription) = 0  ) , _CX_Monitor_Cust AS ( SELECT     CX_Monitor.QuestionDescription      ,CX_Monitor.QuestionKey      ,CX_Monitor.RespondentId      ,CX_Monitor.Data      ,CX_Monitor.Meta_Insert_TaskExecutionInstanceId      ,CX_Monitor.Meta_LatestUpdate_TaskExecutionInstanceId      FROM [Schema].CX_Monitor       WHERE 1=1     AND  CX_Monitor.QuestionKey = 'PARTY' )
+	;WITH _CX_Monitor AS ( SELECT     CX_Monitor.QuestionDescription      ,CX_Monitor.QuestionKey     ,CASE WHEN(CASE WHEN (CHARINDEX('#', CX_Monitor.QuestionKey)) = (LEN(CX_Monitor.QuestionKey)) THEN 1 ELSE 0 END) = 0        THEN REPLACE(SUBSTRING(CX_Monitor.QuestionKey, CHARINDEX(',', CX_Monitor.QuestionKey), LEN(CX_Monitor.QuestionKey)), ',', '')        ELSE CX_Monitor.QuestionKey END AS QuestionId     ,CX_Monitor.RespondentId      ,CX_Monitor.Data      ,CX_Monitor.Meta_Insert_TaskExecutionInstanceId      ,CX_Monitor.Meta_LatestUpdate_TaskExecutionInstanceId     ,SUBSTRING(CX_Monitor.QuestionDescription,CHARINDEX(' ',CX_Monitor.QuestionDescription)+1,LEN(CX_Monitor.QuestionDescription)) AS QuestionRef       FROM [Schema].CX_Monitor      WHERE 1=1     AND  CHARINDEX('Q',CX_Monitor.QuestionDescription) > 0      AND  CHARINDEX('x.',CX_Monitor.QuestionDescription) = 0  ) , _CX_Monitor_Cust AS ( SELECT     CX_Monitor.QuestionDescription      ,CX_Monitor.QuestionKey      ,CX_Monitor.RespondentId      ,CX_Monitor.Data      ,CX_Monitor.Meta_Insert_TaskExecutionInstanceId      ,CX_Monitor.Meta_LatestUpdate_TaskExecutionInstanceId      FROM [Schema].CX_Monitor       WHERE 1=1     AND  CX_Monitor.QuestionKey = 'PARTY'  ) , _Question_Id AS ( SELECT     DimQuestion.QuestionId       ,DimQuestion.QuestionKey      FROM [Schema].DimQuestion )
 	INSERT INTO lumo.FactSurvey (
 		FactSurvey.CustomerId,
 		FactSurvey.QuestionId,
@@ -23,14 +23,14 @@ END
 		FactSurvey.ResponseKey)
 	  SELECT
 		_CX_Monitor_Cust.Data,
-		_CX_Monitor.QuestionRef,
-		CONVERT(DATETIME,CONVERT(CHAR,GETDATE(),103),103) ,
+		_Question_Id.QuestionId,
+		/* _Question_Id.QuestionId */ CONVERT(NCHAR(8),ISNULL(GETDATE(), '9999-12-31'),112),
 		_CX_MonitorCodes.Label,
 		_CX_Monitor.RespondentId,
 		CAST((REPLACE(SUBSTRING( _CX_Monitor.QuestionKey , CHARINDEX(',', _CX_Monitor.QuestionKey), LEN(_CX_Monitor.QuestionKey)), '#', ''))AS VARCHAR(MAX))
   + '' + CAST((REPLACE(SUBSTRING(_CX_Monitor.QuestionKey, CHARINDEX('#', _CX_Monitor.QuestionKey), LEN(_CX_Monitor.QuestionKey)), '#', ''))AS VARCHAR(MAX))
   + '' + CAST(_CX_Monitor.RespondentId AS VARCHAR(MAX))
-	  FROM [Schema].CX_MonitorCodes  _CX_MonitorCodes LEFT JOIN _CX_Monitor ON _CX_MonitorCodes.QuestionKey = _CX_Monitor.QuestionId   AND _CX_MonitorCodes.Value = _CX_Monitor.Data  LEFT JOIN _CX_Monitor_Cust ON _CX_Monitor.RespondentId = _CX_Monitor_Cust.RespondentId  WHERE 1=1  AND _CX_Monitor.QuestionId IS NOT NULL;
+	  FROM lumo.CX_MonitorCodes   LEFT JOIN _CX_Monitor ON CX_MonitorCodes.QuestionKey = _CX_Monitor.QuestionId   AND CX_MonitorCodes.Value = _CX_Monitor.Data  LEFT JOIN _CX_Monitor_Cust ON _CX_Monitor.RespondentId = _CX_Monitor_Cust.RespondentId  LEFT JOIN _Question_Id ON _CX_Monitor.QuestionRef = _Question_Id.QuestionKey   WHERE 1=1  AND _CX_Monitor.QuestionId IS NOT NULL;
 
 SELECT 0 AS ExtractRowCount,
 @@ROWCOUNT AS InsertRowCount,
