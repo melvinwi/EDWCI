64f7799d9925da1f83fed956debb165f4a211329
danielsteinwall <Daniel.Steinwall@lumoenergy.com.au>
2014-10-31 12:34:55 +1100

    DT 601 - 604
    DimQuestion amended.

--- transformations/lumo_TRANSFORM_DimQuestion.sql
+++ transformations/lumo_TRANSFORM_DimQuestion.sql
@@ -16,7 +16,7 @@ END
 	INSERT INTO lumo.DimQuestion (
 		DimQuestion.QuestionKey,
 		DimQuestion.Question)
-	  SELECT
+	  SELECT DISTINCT
 		SUBSTRING( _CX_Monitor.QuestionDescription ,CHARINDEX(' ',_CX_Monitor.QuestionDescription)+1,LEN(_CX_Monitor.QuestionDescription)),
 		SUBSTRING( _CX_Monitor.QuestionDescription ,CHARINDEX(' ',_CX_Monitor.QuestionDescription)+1,LEN(_CX_Monitor.QuestionDescription))
 	  FROM lumo.CX_Monitor AS _CX_Monitor WHERE CHARINDEX('Q',_CX_Monitor.QuestionDescription) > 0 AND CHARINDEX('x.',_CX_Monitor.QuestionDescription) = 0 AND _CX_Monitor.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID;
