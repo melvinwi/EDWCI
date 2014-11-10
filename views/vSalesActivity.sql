0a74a4fec18c296c066a2ae9451addd5437f86cb
Dan Morrison <dan@danm.it>
2014-11-10 16:52:12 +1100

    Add sales activity fact views [#641]

---
+++ views/vSalesActivity.sql
@@ -0,0 +1,64 @@
+CREATE VIEW Views.vSalesActivity
+AS
+SELECT -- DimAccount
+       DimAccountCurrent.AccountCode,
+       DimAccountCurrent.PostalAddressLine1,
+       DimAccountCurrent.PostalSuburb,
+       DimAccountCurrent.PostalPostcode,
+       DimAccountCurrent.PostalState,
+       DimAccountCurrent.PostalStateAsProvided,
+       DimAccountCurrent.AccountStatus,
+       DimAccountCurrent.AccountCreationDate,
+       DimAccountCurrent.AccountClosedDate,
+       DimAccountCurrent.CreditControlStatus,
+       DimAccountCurrent.CreditControlCategory,
+       DimAccountCurrent.InvoiceDeliveryMethod,
+       DimAccountCurrent.PaymentMethod,
+       DimAccountCurrent.MyAccountStatus,
+       DimAccountCurrent.ACN,
+       DimAccountCurrent.ABN,
+       DimAccountCurrent.AccountType,
+       -- DimService
+       DimServiceCurrent.MarketIdentifier,
+       DimServiceCurrent.ServiceType,
+       DimServiceCurrent.LossFactor,
+       DimServiceCurrent.EstimatedDailyConsumption,
+       DimServiceCurrent.MeteringType,
+       DimServiceCurrent.ResidentialSuburb,
+       DimServiceCurrent.ResidentialPostcode,
+       DimServiceCurrent.ResidentialState,
+       DimServiceCurrent.NextScheduledReadDate,
+       DimServiceCurrent.FRMPDate,
+       -- DimProduct
+       DimProductCurrent.ProductName,
+       DimProductCurrent.ProductDesc,
+       DimProductCurrent.ProductType,
+       -- DimPricePlan
+       DimPricePlanCurrent.PricePlanCode,
+       DimPricePlanCurrent.PricePlanName,
+       DimPricePlanCurrent.PricePlanDiscountPercentage,
+       DimPricePlanCurrent.PricePlanValueRatio,
+       -- DimRepresentative
+       DimRepresentativeCurrent.RepresentativePartyName,
+       -- DimOrganisation
+       DimOrganisationCurrent.OrganisationName,
+       DimOrganisationCurrent.Level1Name,
+       DimOrganisationCurrent.Level2Name,
+       DimOrganisationCurrent.Level3Name,
+       DimOrganisationCurrent.Level4Name,
+       -- FactSalesActivity
+       FactSalesActivity.SalesActivityType,
+       CONVERT(DATETIME2, CAST(FactSalesActivity.SalesActivityDateId AS NCHAR(8)) + ' ' + CAST(FactSalesActivity.SalesActivityTime AS NCHAR(16))) AS ActivityDate
+FROM   DW_Dimensional.DW.FactSalesActivity
+LEFT   JOIN DW_Dimensional.DW.DimAccount ON DimAccount.AccountId = FactSalesActivity.AccountId
+LEFT   JOIN DW_Dimensional.DW.DimAccount AS DimAccountCurrent ON DimAccountCurrent.AccountKey = DimAccount.AccountKey AND DimAccountCurrent.Meta_IsCurrent = 1
+LEFT   JOIN DW_Dimensional.DW.DimService ON DimService.ServiceId = FactSalesActivity.ServiceId
+LEFT   JOIN DW_Dimensional.DW.DimService AS DimServiceCurrent ON DimServiceCurrent.ServiceKey = DimService.ServiceKey AND DimServiceCurrent.Meta_IsCurrent = 1
+LEFT   JOIN DW_Dimensional.DW.DimProduct ON DimProduct.ProductId = FactSalesActivity.ProductId
+LEFT   JOIN DW_Dimensional.DW.DimProduct AS DimProductCurrent ON DimProductCurrent.ProductKey = DimProduct.ProductKey AND DimProductCurrent.Meta_IsCurrent = 1
+LEFT   JOIN DW_Dimensional.DW.DimPricePlan ON DimPricePlan.PricePlanId = FactSalesActivity.PricePlanId
+LEFT   JOIN DW_Dimensional.DW.DimPricePlan AS DimPricePlanCurrent ON DimPricePlanCurrent.PricePlanKey = DimPricePlan.PricePlanKey AND DimPricePlanCurrent.Meta_IsCurrent = 1
+LEFT   JOIN DW_Dimensional.DW.DimRepresentative ON DimRepresentative.RepresentativeId = FactSalesActivity.RepresentativeId
+LEFT   JOIN DW_Dimensional.DW.DimRepresentative AS DimRepresentativeCurrent ON DimRepresentativeCurrent.RepresentativeKey = DimRepresentative.RepresentativeKey AND DimRepresentative.Meta_IsCurrent = 1
+LEFT   JOIN DW_Dimensional.DW.DimOrganisation ON DimOrganisation.OrganisationId = FactSalesActivity.OrganisationId
+LEFT   JOIN DW_Dimensional.DW.DimOrganisation AS DimOrganisationCurrent ON DimOrganisationCurrent.OrganisationKey = DimOrganisation.OrganisationKey AND DimOrganisation.Meta_IsCurrent = 1;
---
+++ views/vSalesActivityHistorical.sql
@@ -0,0 +1,58 @@
+CREATE VIEW Views.vSalesActivityHistorical
+AS
+SELECT -- DimAccount
+       DimAccount.AccountCode,
+       DimAccount.PostalAddressLine1,
+       DimAccount.PostalSuburb,
+       DimAccount.PostalPostcode,
+       DimAccount.PostalState,
+       DimAccount.PostalStateAsProvided,
+       DimAccount.AccountStatus,
+       DimAccount.AccountCreationDate,
+       DimAccount.AccountClosedDate,
+       DimAccount.CreditControlStatus,
+       DimAccount.CreditControlCategory,
+       DimAccount.InvoiceDeliveryMethod,
+       DimAccount.PaymentMethod,
+       DimAccount.MyAccountStatus,
+       DimAccount.ACN,
+       DimAccount.ABN,
+       DimAccount.AccountType,
+       -- DimService
+       DimService.MarketIdentifier,
+       DimService.ServiceType,
+       DimService.LossFactor,
+       DimService.EstimatedDailyConsumption,
+       DimService.MeteringType,
+       DimService.ResidentialSuburb,
+       DimService.ResidentialPostcode,
+       DimService.ResidentialState,
+       DimService.NextScheduledReadDate,
+       DimService.FRMPDate,
+       -- DimProduct
+       DimProduct.ProductName,
+       DimProduct.ProductDesc,
+       DimProduct.ProductType,
+       -- DimPricePlan
+       DimPricePlan.PricePlanCode,
+       DimPricePlan.PricePlanName,
+       DimPricePlan.PricePlanDiscountPercentage,
+       DimPricePlan.PricePlanValueRatio,
+       -- DimRepresentative
+       DimRepresentative.RepresentativePartyName,
+       -- DimOrganisation
+       DimOrganisation.OrganisationName,
+       DimOrganisation.Level1Name,
+       DimOrganisation.Level2Name,
+       DimOrganisation.Level3Name,
+       DimOrganisation.Level4Name,
+       -- FactSalesActivity
+       FactSalesActivity.SalesActivityType,
+       CONVERT(DATETIME2, CAST(FactSalesActivity.SalesActivityDateId AS NCHAR(8)) + ' ' + CAST(FactSalesActivity.SalesActivityTime AS NCHAR(16))) AS ActivityDate
+FROM   DW_Dimensional.DW.FactSalesActivity
+LEFT   JOIN DW_Dimensional.DW.DimAccount ON DimAccount.AccountId = FactSalesActivity.AccountId
+LEFT   JOIN DW_Dimensional.DW.DimService ON DimService.ServiceId = FactSalesActivity.ServiceId
+LEFT   JOIN DW_Dimensional.DW.DimProduct ON DimProduct.ProductId = FactSalesActivity.ProductId
+LEFT   JOIN DW_Dimensional.DW.DimPricePlan ON DimPricePlan.PricePlanId = FactSalesActivity.PricePlanId
+LEFT   JOIN DW_Dimensional.DW.DimRepresentative ON DimRepresentative.RepresentativeId = FactSalesActivity.RepresentativeId
+LEFT   JOIN DW_Dimensional.DW.DimOrganisation ON DimOrganisation.OrganisationId = FactSalesActivity.OrganisationId;
