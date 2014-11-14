68fc679b626a88befa7cfca177d3688fb179ab61
danielsteinwall <Daniel.Steinwall@lumoenergy.com.au>
2014-11-14 15:02:00 +1100

    DT602
    Updated FactSurvey to include FileDate column

--- transformations/lumo_TRANSFORM_FactSurvey.sql
+++ transformations/lumo_TRANSFORM_FactSurvey.sql
@@ -13,7 +13,7 @@ EXEC DW_Utility.config.GetLatestSuccessfulTaskExecutionInstanceID
 END
 --/
 
-	;WITH _CX_Monitor AS ( SELECT CX_Monitor.QuestionDescription  ,CX_Monitor.QuestionKey ,CASE WHEN(CASE WHEN (CHARINDEX('#', CX_Monitor.QuestionKey)) = (LEN(CX_Monitor.QuestionKey)) THEN 1 ELSE 0 END) = 0  THEN REPLACE(SUBSTRING(CX_Monitor.QuestionKey, CHARINDEX(',', CX_Monitor.QuestionKey), LEN(CX_Monitor.QuestionKey)), ',', '')  ELSE CX_Monitor.QuestionKey END AS QuestionId ,CX_Monitor.RespondentId  ,CX_Monitor.Data  ,CX_Monitor.Meta_Insert_TaskExecutionInstanceId  ,CX_Monitor.Meta_LatestUpdate_TaskExecutionInstanceId ,SUBSTRING(CX_Monitor.QuestionDescription,CHARINDEX(' ',CX_Monitor.QuestionDescription)+1,LEN(CX_Monitor.QuestionDescription)) AS QuestionRef   FROM lumo.CX_Monitor  WHERE CHARINDEX('Q',CX_Monitor.QuestionDescription) > 0  AND  CHARINDEX('x.',CX_Monitor.QuestionDescription) = 0  ) , _CX_Monitor_Cust AS ( SELECT CX_Monitor.QuestionDescription  ,CX_Monitor.QuestionKey  ,CX_Monitor.RespondentId  ,CX_Monitor.Data  ,CX_Monitor.Meta_Insert_TaskExecutionInstanceId  ,CX_Monitor.Meta_LatestUpdate_TaskExecutionInstanceId  FROM lumo.CX_Monitor   WHERE CX_Monitor.QuestionKey = 'PARTY' )  , _DimCustomer AS ( SELECT DimCustomer.CustomerId ,DimCustomer.CustomerCode  FROM lumo.DimCustomer   WHERE DimCustomer.Meta_IsCurrent = 1 )  , _Question_Id AS ( SELECT DimQuestion.QuestionId   ,DimQuestion.QuestionKey  FROM lumo.DimQuestion )
+	;WITH _CX_Monitor AS ( SELECT CX_Monitor.QuestionDescription  ,CX_Monitor.QuestionKey ,CASE WHEN(CASE WHEN (CHARINDEX('#', CX_Monitor.QuestionKey)) = (LEN(CX_Monitor.QuestionKey)) THEN 1 ELSE 0 END) = 0  THEN REPLACE(SUBSTRING(CX_Monitor.QuestionKey, CHARINDEX(',', CX_Monitor.QuestionKey), LEN(CX_Monitor.QuestionKey)), ',', '')  ELSE CX_Monitor.QuestionKey END AS QuestionId ,CX_Monitor.RespondentId  ,CX_Monitor.Data,CX_Monitor.FileDate  ,CX_Monitor.Meta_Insert_TaskExecutionInstanceId  ,CX_Monitor.Meta_LatestUpdate_TaskExecutionInstanceId ,SUBSTRING(CX_Monitor.QuestionDescription,CHARINDEX(' ',CX_Monitor.QuestionDescription)+1,LEN(CX_Monitor.QuestionDescription)) AS QuestionRef   FROM lumo.CX_Monitor  WHERE CHARINDEX('Q',CX_Monitor.QuestionDescription) > 0  AND  CHARINDEX('x.',CX_Monitor.QuestionDescription) = 0  ) , _CX_Monitor_Cust AS ( SELECT CX_Monitor.QuestionDescription  ,CX_Monitor.QuestionKey  ,CX_Monitor.RespondentId  ,CX_Monitor.Data  ,CX_Monitor.FileDate,CX_Monitor.Meta_Insert_TaskExecutionInstanceId  ,CX_Monitor.Meta_LatestUpdate_TaskExecutionInstanceId  FROM lumo.CX_Monitor   WHERE CX_Monitor.QuestionKey = 'PARTY' )  , _DimCustomer AS ( SELECT DimCustomer.CustomerId ,DimCustomer.CustomerCode  FROM lumo.DimCustomer   WHERE DimCustomer.Meta_IsCurrent = 1 )  , _Question_Id AS ( SELECT DimQuestion.QuestionId   ,DimQuestion.QuestionKey  FROM lumo.DimQuestion )
 	INSERT INTO lumo.FactSurvey (
 		FactSurvey.CustomerId,
 		FactSurvey.QuestionId,
@@ -24,7 +24,7 @@ END
 	  SELECT
 		_DimCustomer.CustomerId,
 		_Question_Id.QuestionId,
-		/* _Question_Id.QuestionId */ CONVERT(NCHAR(8),ISNULL(GETDATE(), '9999-12-31'),112),
+		/* _Question_Id.QuestionId */ CONVERT(NCHAR(8),ISNULL(_CX_Monitor.FileDate, '99991231'),112),
 		COALESCE( _CX_MonitorCodes.Label ,_CX_Monitor.Data) ,
 		_CX_Monitor.RespondentId,
 		CAST((REPLACE(SUBSTRING( _CX_Monitor.QuestionKey , CHARINDEX(',', _CX_Monitor.QuestionKey), LEN(_CX_Monitor.QuestionKey)), '#', ''))AS VARCHAR(MAX))
