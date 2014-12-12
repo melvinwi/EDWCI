db3795c4ff95b6f7849208a81bcef6d32116621e
Dan Morrison <dan@danm.it>
2014-12-12 13:43:07 +1100

    Add transmission node daily load transform [#767]

---
+++ transformations/lumo_TRANSFORM_FactTransmissionNodeDailyLoad.sql
@@ -0,0 +1,55 @@
+CREATE PROCEDURE lumo.TRANSFORM_FactTransmissionNodeDailyLoad
+@TaskExecutionInstanceID INT
+,@LatestSuccessfulTaskExecutionInstanceID INT
+AS
+BEGIN
+
+--Get LatestSuccessfulTaskExecutionInstanceID
+IF  @LatestSuccessfulTaskExecutionInstanceID IS NULL
+BEGIN
+EXEC DW_Utility.config.GetLatestSuccessfulTaskExecutionInstanceID
+@TaskExecutionInstanceID = @TaskExecutionInstanceID
+, @LatestSuccessfulTaskExecutionInstanceID  = @LatestSuccessfulTaskExecutionInstanceID OUTPUT
+END
+--/
+
+	;WITH dimTransmissionNode AS (SELECT DimTransmissionNode.TransmissionNodeId, DimTransmissionNode.TransmissionNodeIdentity, ROW_NUMBER() OVER (PARTITION BY DimTransmissionNode.TransmissionNodeIdentity ORDER BY DimTransmissionNode.TransmissionNodeKey DESC) AS recency FROM /* Dimensional */ lumo.DimTransmissionNode)
+	INSERT INTO lumo.FactTransmissionNodeDailyLoad (
+		FactTransmissionNodeDailyLoad.TransmissionNodeId,
+		FactTransmissionNodeDailyLoad.SettlementDateId,
+		FactTransmissionNodeDailyLoad.SettlementRun,
+		FactTransmissionNodeDailyLoad.Region,
+		FactTransmissionNodeDailyLoad.ImportGrossEnergy,
+		FactTransmissionNodeDailyLoad.ExportGrossEnergy,
+		FactTransmissionNodeDailyLoad.ImportNetEnergy,
+		FactTransmissionNodeDailyLoad.ExportNetEnergy,
+		FactTransmissionNodeDailyLoad.ImportReactivePower,
+		FactTransmissionNodeDailyLoad.ExportReactivePower,
+		FactTransmissionNodeDailyLoad.SettlementAmount,
+		FactTransmissionNodeDailyLoad.MeterRun,
+		FactTransmissionNodeDailyLoad.MeteringDataAgent,
+		FactTransmissionNodeDailyLoad.TransmissionNodeDailyLoadKey)
+	  SELECT
+		COALESCE( _dimTransmissionNode.TransmissionNodeId , 1),
+		CONVERT(NCHAR(8), Settlement_CPDATA.SettlementDate , 112),
+		Settlement_CPDATA.VersionNo,
+		MAX(ISNULL( Settlement_CPDATA.RegionId , '')),
+		SUM(ISNULL( Settlement_CPDATA.IgEnergy , 0.0)),
+		SUM(ISNULL( Settlement_CPDATA.XgEnergy , 0.0)),
+		SUM(ISNULL( Settlement_CPDATA.InEnergy , 0.0)),
+		SUM(ISNULL( Settlement_CPDATA.XnEnergy , 0.0)),
+		SUM(ISNULL( Settlement_CPDATA.Ipower , 0.0)),
+		SUM(ISNULL( Settlement_CPDATA.Xpower , 0.0)),
+		SUM(ISNULL( Settlement_CPDATA.EP , 0.0)),
+		MAX(ISNULL( Settlement_CPDATA.MeterRunNo , 0)),
+		MAX(ISNULL( Settlement_CPDATA.MDA , '')),
+		CONVERT(NCHAR(8), Settlement_CPDATA.SettlementDate , 112) + '.' + CAST(Settlement_CPDATA.VersionNo AS NVARCHAR(10)) + '.' + Settlement_CPDATA.ParticipantId + '.' + Settlement_CPDATA.TcpId + '.' + Settlement_CPDATA.MDA
+	  FROM /* Staging */ lumo.Settlement_CPDATA LEFT JOIN dimTransmissionNode AS _dimTransmissionNode ON _dimTransmissionNode.TransmissionNodeIdentity = Settlement_CPDATA.TcpId AND _dimTransmissionNode.recency = 1 WHERE Settlement_CPDATA.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID GROUP BY Settlement_CPDATA.SettlementDate, Settlement_CPDATA.VersionNo, Settlement_CPDATA.ParticipantId, Settlement_CPDATA.TcpId, Settlement_CPDATA.MDA, _dimTransmissionNode.TransmissionNodeId;
+
+SELECT 0 AS ExtractRowCount,
+@@ROWCOUNT AS InsertRowCount,
+0 AS UpdateRowCount,
+0 AS DeleteRowCount,
+0 AS ErrorRowCount;
+
+END;
