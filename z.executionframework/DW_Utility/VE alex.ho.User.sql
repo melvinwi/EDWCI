f85f26ba6829281edc545a746823e27adca05c40
jonmgiles <jon.m.giles@gmail.com>
2014-12-16 11:46:25 +1100

    Delete DB objects (that are no longer in Testing3)

--- z.executionframework/DW_Utility/VE alex.ho.User.sql
+++
@@ -1,8 +0,0 @@
-USE [DW_Utility]
-GO
-CREATE USER [VE\alex.ho] FOR LOGIN [VE\alex.ho] WITH DEFAULT_SCHEMA=[dbo]
-GO
-ALTER ROLE [db_datareader] ADD MEMBER [VE\alex.ho]
-GO
-ALTER ROLE [db_denydatawriter] ADD MEMBER [VE\alex.ho]
-GO
