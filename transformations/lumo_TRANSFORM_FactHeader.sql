544ae5781e990fa4e902eae3113ffc95ee63ae58
Dan Morrison <dan@danm.it>
2015-02-20 17:59:30 +1100

    Add additional header columns

--- transformations/lumo_TRANSFORM_FactHeader.sql
+++ transformations/lumo_TRANSFORM_FactHeader.sql
@@ -15,17 +15,29 @@ END
 
 	INSERT INTO lumo.FactHeader (
 		FactHeader.AccountId,
+		FactHeader.PostedDateId,
 		FactHeader.IssueDateId,
 		FactHeader.DueDateId,
 		FactHeader.HeaderType,
+		FactHeader.Invoice,
+		FactHeader.Tax,
+		FactHeader.PromptPaymentDiscount,
 		FactHeader.PaidOnTime,
+		FactHeader.Reversal,
+		FactHeader.InvoiceBatch,
 		FactHeader.HeaderKey)
 	  SELECT
 		_DimAccount.AccountId,
+		CONVERT(NCHAR(8), COALESCE( ar_invoice.posted_date , '9999-12-31'), 112),
 		CONVERT(NCHAR(8), COALESCE( inv_invoice_header.invoice_date , '9999-12-31'), 112),
 		CONVERT(NCHAR(8), COALESCE( ar_invoice.invoice_due_date , '9999-12-31'), 112),
 		/* inv_invoice_header.invoice_reference */ 'Invoice',
+		inv_invoice_header.invoice_amount,
+		inv_invoice_header.tax_amount,
+		inv_invoice_header.prompt_payment_disc,
 		CASE ar_invoice.paid_promptly WHEN 'Y' THEN 'Yes' ELSE 'No' END,
+		CASE ar_invoice.reversal WHEN 'Y' THEN 'Yes' ELSE 'No' END,
+		inv_invoice_header.seq_invoice_run_id,
 		inv_invoice_header.invoice_reference
 	  FROM /* Staging */ lumo.inv_invoice_header INNER JOIN /* Staging */ lumo.ar_invoice ON ar_invoice.seq_ar_invoice_id = inv_invoice_header.seq_invoice_header_id INNER JOIN /* Staging */ lumo.nc_client ON nc_client.seq_party_id = ar_invoice.seq_party_id INNER JOIN /* Staging */ lumo.crm_element_hierarchy ON crm_element_hierarchy.element_id = nc_client.seq_party_id AND crm_element_hierarchy.element_struct_code = 'CLIENT' INNER JOIN /* Staging */ lumo.crm_party_type ON crm_party_type.seq_party_type_id = crm_element_hierarchy.seq_element_type_id AND crm_party_type.party_type <> 'SUBCLIENT' INNER JOIN /* Dimensional */ lumo.DimAccount AS _DimAccount ON _DimAccount.AccountKey = nc_client.seq_party_id AND _DimAccount.Meta_IsCurrent = 1 WHERE inv_invoice_header.seq_invoice_run_id <> 0 AND (inv_invoice_header.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID OR ar_invoice.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID);
 
