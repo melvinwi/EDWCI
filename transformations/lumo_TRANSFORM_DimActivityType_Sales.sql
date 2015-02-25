776019fd813823f8a2c365d0b017e0d106941a11
danielsteinwall <Daniel.Steinwall@lumoenergy.com.au>
2015-02-26 10:37:34 +1100

    Update DimActivityType_Sales BIU6860

--- transformations/lumo_TRANSFORM_DimActivityType_Sales.sql
+++ transformations/lumo_TRANSFORM_DimActivityType_Sales.sql
@@ -19,11 +19,11 @@ END
 		DimActivityType.ActivityTypeDesc,
 		DimActivityType.ActivityCategory)
 	  SELECT
-		CAST('SAT'+''+CAST( tbl_3_132_EN.[uda_132_2922] AS NVARCHAR(20))AS nvarchar(20)),
-		CAST( tbl_3_132_EN.[uda_132_2922] AS nvarchar(20)),
+		CAST('SAT'+''+CAST( nc_involvement_type.seq_involve_type_id AS NVARCHAR(20))AS nvarchar(20)),
+		CAST( nc_involvement_type.seq_involve_type_id AS nvarchar(20)),
 		CAST( nc_involvement_type.involve_type_desc AS nvarchar(100)),
-		CAST( tbl_3_131_EN.name AS nvarchar(100))
-	  FROM lumo.tbl_3_132_EN INNER JOIN lumo.nc_involvement_type ON tbl_3_132_EN.uda_132_2922 = nc_involvement_type.seq_involve_type_id LEFT JOIN lumo.tbl_3_131_EN ON tbl_3_132_EN.uda_132_2923 = tbl_3_131_EN.Code WHERE tbl_3_132_EN.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID OR tbl_3_131_EN.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID OR nc_involvement_type.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID;
+		CAST(ISNULL( tbl_3_131_EN.name ,'Undefined')AS nvarchar(100))
+	  FROM lumo.nc_involvement_type LEFT JOIN lumo.tbl_3_132_EN ON nc_involvement_type.seq_involve_type_id = tbl_3_132_EN.uda_132_2922 LEFT JOIN lumo.tbl_3_131_EN ON tbl_3_132_EN.uda_132_2923 = tbl_3_131_EN.Code WHERE tbl_3_132_EN.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID OR tbl_3_131_EN.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID OR nc_involvement_type.Meta_LatestUpdate_TaskExecutionInstanceId > @LatestSuccessfulTaskExecutionInstanceID;
 
 SELECT 0 AS ExtractRowCount,
 @@ROWCOUNT AS InsertRowCount,
